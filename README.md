3508b7252731bd9e6f103f570bdf11b8031d779a
Charles Chege <charleschege@protonmail.ch>
2019-05-02 11:11:02 +0300

    restructure to create library and generate random memorable urls

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-## passphrase
+## passphrase_lib
 
 Passhrase is a pure Rust crate focused on generating solid passphrases at the touch of a button , giving users the ability to generate memorable strong passphrases that takes thousands of years to crack using specialized password cracking computers or taking forever for normal computers to crack.
 
 At the moment(an English only version is coming soon), it combines English and Swahili dictionaries of short easy to type words.
-The `zxcvbn` crate, a password strength estimator based off of Dropbox's zxcvbn library, is used to calculate how long it would take to crack the password, the number of guesses it would need and the number of years it would take to crack the passphrase.
+The `zxcvbn` crate, a password strength estimator based off of Dropbox's zxcvbn library, has been used to counter-check how long it would take to crack the password, the number of guesses it would need and the number of years it would take to crack the passphrase.
 
 However, kindly node that this is only a passphrase generator and you need to hash it with a good hashing algorithm (I recommend argon2 implementations) for use to store in a database
 
@@ -11,10 +11,11 @@ However, kindly node that this is only a passphrase generator and you need to ha
 
 ---
 
-use `cargo install passphrase` to install it on your machine.
+use `cargo add passphrase` to install to add to your `Cargo.toml` file.
 
-Run it via the commandline as
-
-$ `passhrase`
-
+To add it manually to `Cargo.toml` file.
+```
+[dependencies]
+passhrase_lib = #enter the version from crates.io here
+```
 ---
---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+//! Passhrase is a pure Rust crate focused on generating solid passphrases at the touch of a button
+//! , giving users the ability to generate memorable strong passphrases that takes thousands of
+//! years to crack using specialized password cracking computers or taking forever for normal
+//! computers to crack.
+//!
+//!At the moment(an English only version is coming soon), 
+//! it combines English and Swahili dictionaries of short easy to type words.
+//! The `zxcvbn` crate, a password strength estimator based off of Dropbox's zxcvbn library, 
+//!has been used to counter-check how long it would take to crack the password, 
+//!the number of guesses it would need and the number of years it would take to crack the passphrase.
+//!
+//! However, kindly node that this is only a passphrase generator and you need to hash it with
+//! a good hashing algorithm (I recommend argon2 implementations) for use to store in a database
+//!
+//! [WARNING!!!] NEVER STORE THE PLAIN TEXT VERSION OF A PASSPHRASE
+//!
+//#---
+
+//# use `cargo add passphrase` to install to add to your `Cargo.toml` file.
+
+//# To add it manually to `Cargo.toml` file.
+//# ```
+//# [dependencies]
+//# passhrase_lib = #enter the version from crates.io here
+//# ```
+//#---
+
+mod dictionary;
+
+use {
+    rand::{Rng, thread_rng},
+    crate::dictionary::*,
+};
+
+fn choose_a_word(data_type: &'static[&'static str]) -> &str {
+    let size = data_type.len();
+    let data_type = data_type[thread_rng().gen_range(0, size)];
+    data_type
+}
+
+pub fn gen_passphrase() -> String {
+    let first_iteration = SWAHILI;
+    let second_iteration = ENGLISH;
+    let third_iteration = SWAHILI;
+    let fourth_iteration = ENGLISH;
+
+    let passphr = format!("{} {} {} {}", choose_a_word(second_iteration), choose_a_word(first_iteration),  choose_a_word(fourth_iteration), choose_a_word(third_iteration));
+    
+    passphr
+
+}
+
+pub fn gen_url() -> String {
+    let first_iteration = SWAHILI;
+    let second_iteration = ENGLISH;
+    let fourth_iteration = ENGLISH;
+
+    let passphr = format!("{}-{}-{}", choose_a_word(second_iteration), choose_a_word(first_iteration),  choose_a_word(fourth_iteration));
+    
+    passphr
+}
